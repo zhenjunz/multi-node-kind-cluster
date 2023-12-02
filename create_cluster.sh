2b3315a1d37a9e1048d51768c9d3e987e1437399
zzhuo <zzhuo@beijing.local>
2023-12-02 09:47:45 +0800

    add mirror commands

--- create_cluster.sh
+++ create_cluster.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "\n*******************************************************************************************************************"
 echo "Installing Kind"
 echo "*******************************************************************************************************************"
-brew update && brew install kind
+#brew update && brew install kind
 
 echo "\n*******************************************************************************************************************"
 echo "Creating new kind cluster"
@@ -14,13 +14,17 @@ kind create cluster --config "$SCRIPT_DIR"/config.yaml --name dev
 echo "\n*******************************************************************************************************************"
 echo "Installing Calico"
 echo "*******************************************************************************************************************"
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml
+kubectl create -f mirror_tigera_operator.yaml
+kubectl create -f mirror_custom_resources.yaml
+#kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+#kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml
 
 echo "\n*******************************************************************************************************************"
 echo "Installing Nginx Ingress Controller"
 echo "*******************************************************************************************************************"
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+# https://github.com/anjia0532/gcr.io_mirror
+kubectl apply -f mirror_ingress_deploy.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
 echo "\n*******************************************************************************************************************"
 echo "Labeling work node"
